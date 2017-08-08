4d3a641a9cd36cb83e9e3533fc3315bd13b3053f
Nick Wise <nickcwise@gmail.com>
2017-08-08 13:35:58 -0400

    Initial

---
+++ Jackpot/Scripts/script.js
@@ -0,0 +1,23 @@
+﻿
+function getRando(number) {
+    var num = Math.round(Math.random() * number);
+    num = num.toString();
+
+    if (num.length < 2) {
+        num = "0" + num;
+    }
+
+    return num;
+}
+    
+
+function getPBnums() {
+    document.getElementById("PB1").innerHTML = getRando(65);
+    document.getElementById("PB2").innerHTML = getRando(65);
+    document.getElementById("PB3").innerHTML = getRando(65);
+    document.getElementById("PB4").innerHTML = getRando(65);
+    document.getElementById("PB5").innerHTML = getRando(65);
+    document.getElementById("PB6").innerHTML = getRando(65);
+
+
+}
